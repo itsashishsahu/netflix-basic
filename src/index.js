246480b6f22f3938b3d70c1b0918f6b9d7f50bfd
itsashishsahu <itsashishsahu.inc@gmail.com>
2022-01-11 00:46:12 +0530

    added props and 5 web series from 2018.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+
+ReactDOM.render(
+  <>
+    {/* Sacred Games  */}
+    <Card
+      imgsrc="https://m.media-amazon.com/images/M/MV5BYTMzYTQ1NzAtOGY4OC00ZGM5LThiMmUtMzgzZjgzNzNjZjJmXkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_.jpg"
+      title="A Netflix Orginal Series"
+      sname="Sacred Games"
+      link="https://www.netflix.com/in/title/80115328"
+    />
+
+    {/* Narcos  */}
+    <Card
+      imgsrc="https://s3.gaming-cdn.com/images/products/4811/orig/game-steam-narcos-rise-of-the-cartels-cover.jpg"
+      title="A Netflix Orginal Series"
+      sname="Narcos"
+      link="https://www.netflix.com/in/title/80025172"
+    />
+
+    {/* Sex Education  */}
+    <Card
+      imgsrc="https://images-na.ssl-images-amazon.com/images/I/71fh4ioikWL.jpg"
+      title="A Netflix Orginal Series"
+      sname="Sex Education"
+      link="https://www.netflix.com/in/title/80197526"
+    />
+
+    {/* Mirzapur  */}
+    <Card
+      imgsrc="https://m.media-amazon.com/images/M/MV5BN2NlNGYwYTUtMTczMi00NGVjLTgwMzUtNjBkZjIyNDc2NjcxXkEyXkFqcGdeQXVyODQ5NDUwMDk@._V1_.jpg"
+      title="Amazon Orginal"
+      sname="Mirzapur"
+      link="https://www.primevideo.com/detail/Mirzapur/0PDOKMV9CRLOMO5EUKNCUJLG4Q"
+    />
+
+    {/* Apharan  */}
+    <Card
+      imgsrc="https://m.media-amazon.com/images/M/MV5BODQzYjNjNTItZmU2NC00OGE1LTgwNWItZDcxMGE4MmRlMmM3XkEyXkFqcGdeQXVyODI2MzM0MTM@._V1_.jpg"
+      title="ALT Balaji Orginal"
+      sname="Apharan"
+      link="https://www.altbalaji.com/show/apharan/246"
+    />
+  </>,
+  document.getElementById("root")
+);
