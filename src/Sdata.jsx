68097323b06538075a1c002a962b81f9c6415dd6
itsashishsahu <itsashishsahu.inc@gmail.com>
2022-01-11 01:34:26 +0530

    Added CSS, Sdata array for storing data.

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BYTMzYTQ1NzAtOGY4OC00ZGM5LThiMmUtMzgzZjgzNzNjZjJmXkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_.jpg",
+        title: "A Netflix Orginal Series",
+        sname: "Sacred Games",
+        link: "https://www.netflix.com/in/title/80115328"
+    },
+    {
+        imgsrc: "https://s3.gaming-cdn.com/images/products/4811/orig/game-steam-narcos-rise-of-the-cartels-cover.jpg",
+        title: "A Netflix Orginal Series",
+        sname: "Narcos",
+        link: "https://www.netflix.com/in/title/80025172"
+    },
+    { 
+        imgsrc: "https://images-na.ssl-images-amazon.com/images/I/71fh4ioikWL.jpg",
+        title: "A Netflix Orginal Series",
+        sname: "Sex Education",
+        link: "https://www.netflix.com/in/title/80197526"
+    },
+    { 
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BN2NlNGYwYTUtMTczMi00NGVjLTgwMzUtNjBkZjIyNDc2NjcxXkEyXkFqcGdeQXVyODQ5NDUwMDk@._V1_.jpg",
+        title: "Amazon Orginal",
+        sname: "Mirzapur",
+        link: "https://www.primevideo.com/detail/Mirzapur/0PDOKMV9CRLOMO5EUKNCUJLG4Q"
+    },
+    {
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BODQzYjNjNTItZmU2NC00OGE1LTgwNWItZDcxMGE4MmRlMmM3XkEyXkFqcGdeQXVyODI2MzM0MTM@._V1_.jpg",
+        title: "ALT Balaji Orginal",
+        sname: "Apharan",
+        link: "https://www.altbalaji.com/show/apharan/246"
+    }
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Card";
+import Sdata from './Sdata';
 
 ReactDOM.render(
   <>
@@ -10,42 +11,42 @@ ReactDOM.render(
 
     {/* Sacred Games  */}
     <Card
-      imgsrc="https://m.media-amazon.com/images/M/MV5BYTMzYTQ1NzAtOGY4OC00ZGM5LThiMmUtMzgzZjgzNzNjZjJmXkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_.jpg"
-      title="A Netflix Orginal Series"
-      sname="Sacred Games"
-      link="https://www.netflix.com/in/title/80115328"
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      sname={Sdata[0].sname}
+      link={Sdata[0].link}
     />
 
     {/* Narcos  */}
     <Card
-      imgsrc="https://s3.gaming-cdn.com/images/products/4811/orig/game-steam-narcos-rise-of-the-cartels-cover.jpg"
-      title="A Netflix Orginal Series"
-      sname="Narcos"
-      link="https://www.netflix.com/in/title/80025172"
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      sname={Sdata[1].sname}
+      link={Sdata[1].link}
     />
 
     {/* Sex Education  */}
     <Card
-      imgsrc="https://images-na.ssl-images-amazon.com/images/I/71fh4ioikWL.jpg"
-      title="A Netflix Orginal Series"
-      sname="Sex Education"
-      link="https://www.netflix.com/in/title/80197526"
+      imgsrc={Sdata[2].imgsrc}
+      title={Sdata[2].title}
+      sname={Sdata[2].sname}
+      link={Sdata[2].link}
     />
 
     {/* Mirzapur  */}
     <Card
-      imgsrc="https://m.media-amazon.com/images/M/MV5BN2NlNGYwYTUtMTczMi00NGVjLTgwMzUtNjBkZjIyNDc2NjcxXkEyXkFqcGdeQXVyODQ5NDUwMDk@._V1_.jpg"
-      title="Amazon Orginal"
-      sname="Mirzapur"
-      link="https://www.primevideo.com/detail/Mirzapur/0PDOKMV9CRLOMO5EUKNCUJLG4Q"
+      imgsrc={Sdata[3].imgsrc}
+      title={Sdata[3].title}
+      sname={Sdata[3].sname}
+      link={Sdata[3].link}
     />
 
     {/* Apharan  */}
     <Card
-      imgsrc="https://m.media-amazon.com/images/M/MV5BODQzYjNjNTItZmU2NC00OGE1LTgwNWItZDcxMGE4MmRlMmM3XkEyXkFqcGdeQXVyODI2MzM0MTM@._V1_.jpg"
-      title="ALT Balaji Orginal"
-      sname="Apharan"
-      link="https://www.altbalaji.com/show/apharan/246"
+      imgsrc={Sdata[4].imgsrc}
+      title={Sdata[4].title}
+      sname={Sdata[4].sname}
+      link={Sdata[4].link}
     />
   </>,
   document.getElementById("root")
